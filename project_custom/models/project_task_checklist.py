457f7fa4f46a5c5f1db9b36ddcaa5526551a3424
Faiyazi <ibjifaiyazz@gmail.com>
2025-09-18 11:59:27 +0530

    update project custom

--- project_custom/models/project_task_checklist.py
+++ project_custom/models/project_task_checklist.py
@@ -8,12 +8,12 @@ class ProjectTaskChecklist(models.Model):
     _name = 'project.task.checklist'
     _description = 'Project Task Checklist'
     _inherits = {'project.task': 'task_id'}
-
-    task_id = fields.Many2one('project.task', required=True, ondelete='cascade')
-
     _sql_constraints = [
     ('project_task_checklist_unique_email', 'unique(email)', 'Email must be unique!')
         ]
+
+
+    task_id = fields.Many2one('project.task', required=True, ondelete='cascade')
 
 
 
