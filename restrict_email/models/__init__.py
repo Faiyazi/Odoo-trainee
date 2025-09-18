cf3647f7739bf57c457e86d9f9d1842a9b802a17
Faiyazi <ibjifaiyazz@gmail.com>
2025-09-18 12:45:56 +0530

    Updated the restrict email

--- restrict_email/models/__init__.py
+++ restrict_email/models/__init__.py
@@ -1,3 +1,2 @@
-from . import restrict_mail
 from . import res_partner
 from . import mail_thread
--- restrict_email/models/restrict_mail.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class RestrictMail(models.Model):
-    _name = 'mail.restrict'
-
-    
-
-
