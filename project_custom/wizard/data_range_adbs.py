ae28a6f2de90091327c67c90c63c059c4662e2d2
Faiyazi <ibjifaiyazz@gmail.com>
2025-09-18 12:17:20 +0530

    dalete data_range_adbs.3.py

--- project_custom/wizard/data_range_adbs.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class DataTimeSheet(models.AbstractModel):
-    _name = 'TimeSheetata'
-    
-    def data_time_sheet(self, date_start, date_end):
-        for rec in self.env['account.analytic.line'].search([('date', '>=', date_start), ('date', '<=', date_end)]):
-            tasks = rec.mapped('task_id')
-        return tasks
