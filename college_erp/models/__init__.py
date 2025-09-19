607a4f008c03989281c430303a0abc3b3eae4f8a
Chiragbharada007 <chiragbharda8775@gmail.com>
2025-09-19 19:04:26 +0530

    [Remove] remove extra file

--- college_erp/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import person_base
-from . import college_student_admission
-from . import college_teacher
-from . import college_staff
-from . import student_search_wizard
-
