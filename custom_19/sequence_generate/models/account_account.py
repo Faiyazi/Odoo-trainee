7895d7d8eb4d3eceae5ab3cca0c989c64b07f440
Ronak-Patel26 <ronak@email.com>
2025-11-17 18:43:45 +0530

    [MOD]Changes based on requirements

--- custom_19/sequence_generate/models/account_account.py
+++ custom_19/sequence_generate/models/account_account.py
@@ -1,33 +1,35 @@
 from odoo import models, api
 
-
 class AccountAccount(models.Model):
     _inherit = "account.account"
-
 
     @api.onchange("account_type")
     def _onchange_account_type(self):
 
-        mapping = {
-            'asset_receivable': '100000',
-            'asset_cash': '200000',
-            'asset_current': '300000',
-            'asset_non_current': '400000',
+        group_mapping = {
+            'asset': '200001',
+            'liability': '100001',
+            'income': '400001',
+            'expense': '500001',
         }
 
         if self.account_type:
-            default_code = mapping.get(self.account_type)
-            if default_code:
-                # Generate next sequence number by checking existing accounts
-                last_account = self.search([
-                    ('account_type', '=', self.account_type),
-                    ('code', 'like', default_code[:1] + '%')
-                ], order="code desc", limit=1)
-                print("\n\n\n\n\n=============>",default_code[:1])
+            main_group = self.account_type.split('_')[0]
 
-                if last_account and last_account.code.isdigit():
-                    next_code = str(int(last_account.code) + 1).zfill(6)
-                else:
-                    next_code = default_code
+            default_code = group_mapping.get(main_group)
+            if not default_code:
+                return
 
-                self.code = next_code
+            prefix = default_code[0]
+
+            last_account = self.search([
+                ('account_type', 'like', main_group + '%'),
+                ('code', 'like', prefix + '%'),
+            ], order="code desc", limit=1)
+
+            if last_account and last_account.code.isdigit():
+                next_code = str(int(last_account.code) + 1).zfill(6)
+            else:
+                next_code = default_code
+
+            self.code = next_code
